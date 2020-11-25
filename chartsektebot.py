4206f961cd20b11b29aff8f1ed9473404f38564d
Luca Debian <luec.77@gmx.de>
2020-11-25 10:25:31 +0100

    Remove old Functiond / Check IF Guild/Channel

--- chartsektebot.py
+++ chartsektebot.py
@@ -22,17 +22,20 @@ def checkGuild(ID):
     else:
         return False
 
+def checkDM(Channdel_Object):
+    if type(Channdel_Object) is discord.channel.TextChannel:
+        return True
+    else:
+        return False
 
 bot = commands.Bot(command_prefix='>', description="This is a Helper Bot")
 
 @bot.command()
 async def ping(ctx):
-    print(ctx.message)
-    await ctx.send('pong')
-
-@bot.command()
-async def sum(ctx, numOne: int, numTwo: int):
-    await ctx.send(numOne + numTwo)
+    if checkDM(ctx.message.channel):
+        await ctx.send('pong')
+    else:
+        return
 
 # Events
 @bot.event
@@ -51,46 +54,44 @@ async def on_raw_reaction_add(payload):#
     channel = bot.get_channel(cfg["OutputChannel"])
     msg = await bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
 
-    if payload.member.id == msg.author.id:
-        await channel.send(f"User <@{payload.member.id}> du kannst dich nicht selbst voten.")
+    #CHECK EMOJI
+    if payload.emoji.name != cfg["Emoji"]:
         return
-    if payload.emoji.name == cfg["Emoji"]:
-        #CHECK LAST UPVOTE
-        DBCursor.execute("select 'yes' as Result from last_upvote where USER_ID = '%s' AND current_timestamp() > DATE_ADD(UPVOTE_DATE, INTERVAL %s MINUTE);",(payload.member.id,minutes))
+
+    #CHECK SELFVOTE
+    if payload.member.id == msg.author.id:
+        await channel.send(f"**[PRIVATE]** User <@{payload.member.id}> du kannst dich nicht selbst voten.")
+        return
+
+    #CHECK LAST UPVOTE
+    DBCursor.execute("select 'yes' as Result from last_upvote where USER_ID = '%s' AND current_timestamp() > DATE_ADD(UPVOTE_DATE, INTERVAL %s MINUTE);",(payload.member.id,minutes))
+    res = DBCursor.fetchall()
+    try:
+        res = res[0][0]
+    except IndexError:
+        #IF NOT IN DB, THEN RES=YES ELSE NOTHING
+        DBCursor.execute(f"select 'yes' as Result from last_upvote where USER_ID = {payload.member.id};")
         res = DBCursor.fetchall()
         try:
             res = res[0][0]
+            res = "false"
         except IndexError:
-            #IF NOT IN DB, THEN RES=YES ELSE NOTHING
-            DBCursor.execute(f"select 'yes' as Result from last_upvote where USER_ID = {payload.member.id};")
-            res = DBCursor.fetchall()
-            try:
-                res = res[0][0]
-                res = "false"
-            except IndexError:
-                res = "yes"
+            res = "yes"
             
-        if res == "yes":
-            #INSERT LAST UPVOTE
-            DBCursor.execute(f"INSERT INTO last_upvote (USER_ID, UPVOTE_DATE) VALUES ({payload.member.id}, current_timestamp()) ON DUPLICATE KEY UPDATE UPVOTE_DATE = current_timestamp()")
-            DB.commit()
-            DBCursor.execute(f"INSERT INTO upvote (USER_ID, UPVOTE_DATE, UPVOTE, VONUSER_ID) VALUES ({msg.author.id}, current_timestamp(), 1, {payload.member.id}) ON DUPLICATE KEY UPDATE UPVOTE_DATE = current_timestamp()")
-            DB.commit()
-            #INSERT UPVOTE
-            await channel.send(f"<@{msg.author.id}> hat einen Upvote von {payload.member.name} bekommen")
-        else:
-            await channel.send(f"<@{payload.member.id}> du hast erst kürzlich gevotet. Zwischen jedem Vote müssen {minutes} Minuten liegen.")
-            #await payload.member.send(f"User <@{payload.member.id}> du hast erst kürzlich gevotet. Zwischen jedem Vote müssen {minutes} Minuten liegen.")
+    if res == "yes":
+        #INSERT LAST UPVOTE
+        DBCursor.execute(f"INSERT INTO last_upvote (USER_ID, UPVOTE_DATE) VALUES ({payload.member.id}, current_timestamp()) ON DUPLICATE KEY UPDATE UPVOTE_DATE = current_timestamp()")
+        DB.commit()
+        DBCursor.execute(f"INSERT INTO upvote (USER_ID, UPVOTE_DATE, UPVOTE, VONUSER_ID) VALUES ({msg.author.id}, current_timestamp(), 1, {payload.member.id}) ON DUPLICATE KEY UPDATE UPVOTE_DATE = current_timestamp()")
+        DB.commit()
+        #INSERT UPVOTE
+        await channel.send(f"<@{msg.author.id}> hat einen Upvote von {payload.member.name} bekommen")
+    else:
+        await channel.send(f"**[PRIVATE]** <@{payload.member.id}> du hast erst kürzlich gevotet. Zwischen jedem Vote müssen {minutes} Minuten liegen.")
+        #await payload.member.send(f"User <@{payload.member.id}> du hast erst kürzlich gevotet. Zwischen jedem Vote müssen {minutes} Minuten liegen.")
 
-            #mycursor.execute()
-            
-            
-            #if not isinstance(reaciton.message.channel, discord.DMChannel):
-async def on_reaction_add(ctx, reaciton):
-    print(ctx)
-    print(reaciton)
-    print("hallo2")
-    
+        #mycursor.execute()
+
 async def on_message(message):
     if "tutorial" in message.content.lower():
         # in this case don't respond with the word "Tutorial" or you will call the on_message event recursively
